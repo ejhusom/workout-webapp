c33f423c838f557db4ba7bc37344ef791cc15e60
ejhusom <ejlgkvam@online.no>
2020-05-13 16:13:34 +0200

    Add map

--- js/workout-plot.js
+++ js/workout-plot.js
@@ -32,14 +32,16 @@ document.getElementById('import').onclick = function() {
             alt.push(samples[i].ALT);
         }
 
-        function makeTrace(yData, name, yAxis) {
+        function makeTrace(yData, name, yAxis=null) {
             var trace = {
                 x: hour,
                 y: yData,
                 name: name,
                 type: 'scatter',
-                yaxis: yAxis
             };
+            if (yAxis != null) {
+                trace.yaxis = yAxis;
+            }
             return trace;
         }
 
@@ -69,8 +71,53 @@ document.getElementById('import').onclick = function() {
         }
         Plotly.newPlot('mainPlot', data, layout);
 
-        var formatted = JSON.stringify(result, null, 2);
-        document.getElementById('result').value = formatted;
+        var mapTrace = {
+            x: lat,
+            y: lon,
+            name: "map"
+        };
+
+        var mapData = [mapTrace];
+        Plotly.newPlot('mapPlot', mapData);
+
+        var map = L.map('map');//.setView([60.14, 10.25], 11);
+//         L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     id: 'mapbox/streets-v11',
+//     tileSize: 512,
+//     zoomOffset: -1,
+//     accessToken: 'your.mapbox.access.token'
+// }).addTo(mymap);
+L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    maxZoom: 17,
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+}).addTo(map);
+
+        // L.tileLayer('https://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=topo4&zoom={z}&x={x}&y={y}', {
+        //     attribution: '<a href="http://www.kartverket.no/">Kartverket</a>'
+        // }).addTo(map);
+        // var circle = L.circle([60.0, 10.11], {
+        //     color: 'red',
+        //     fillColor: '#f03',
+        //     fillOpacity: 0.5,
+        //     radius: 500
+        // }).addTo(map);
+        // var gpx = '20200307-rondane/activity-20200307.gpx'; 
+        // new L.GPX(gpx, {
+        //     async: true,
+        //     marker_options: {
+        //         startIconUrl: '../images/pin-icon-start.png',
+        //         endIconUrl:   '../images/pin-icon-end.png',
+        //         shadowUrl:    '../images/pin-shadow.png',
+        //         //clickable: true,
+        //         //showRouteInfo: true
+        //     },
+        // }).on('loaded', function(e) {
+        //     map.fitBounds(e.target.getBounds());
+        // }).addTo(map);
+
+
     }
     reader.readAsText(files.item(0));
 }
